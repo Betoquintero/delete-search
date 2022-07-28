feac883a1f08c87aa4d7ae24b2a2a889fed42887
Alberto <aquintero88@gmail.com>
2022-07-28 19:43:28 +0200

    excercise done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,30 @@ import productData from './products.json';
 function App() {
   const [products, setProducts] = useState(productData);
 
+  const handleDelete = (id) => {
+    const filtered = products.filter(elem => elem._id !== id)
+    setProducts(filtered)
+  }
+
+  const handleSearch = (e) => {
+    if  (e.target.value === ''){
+      setProducts(productData)
+    } else {
+      const filtered = products.filter(elem => elem.name.toLowerCase().includes(e.target.value))
+      setProducts(filtered)
+    }
+  }
+
   return (
     <div className="cart">
       <h1>My shopping cart</h1>
+      <input type="text" placeholder='search' onChange={handleSearch}/>
+      {products.map((elem) => {
+        return (<div className="products" key={elem._id}>
+        <p>{elem.name}</p>
+        <button onClick={() => handleDelete(elem._id)}>Delete</button>
+        </div>)
+      })}
 
       {/* Your code goes here */}
 
